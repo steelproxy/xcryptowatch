8ad6d40a35b4ac21d9c356bff36763df9a334784
Collin Rodes <70236495+steelproxy@users.noreply.github.com>
2025-02-04 23:47:16 -0500

    added exception handling

--- xcryptowatch/postal.py
+++ xcryptowatch/postal.py
@@ -5,9 +5,15 @@ def send_analysis(analysis, config):
     logger.info(f"Sending analysis to Postal API...")
     logger.debug(f"Server: {postalsend._app._get_server()} Key: {postalsend._app._get_api_key()}")
     subject = config['postal']['subject']
-    postalsend.push_send(subject, tag=None, plain_body=analysis, html_body=None, attachments=None)
+    try:
+        postalsend.push_send(subject, tag=None, plain_body=analysis, html_body=None, attachments=None)
+    except postalsend.errors.PostalError as e:
+        logger.error(f"Error sending analysis to Postal API: {e}")
 
 def status_update(status):
     logger.info(f"Sending status update to Postal API...")
     logger.debug(f"Server: {postalsend._app._get_server()} Key: {postalsend._app._get_api_key()}")
-    postalsend.push_send("XCryptoWatch Status Update", tag=None, plain_body=("STATUS UPDATE: " + status), html_body=None, attachments=None)
+    try:
+        postalsend.push_send("XCryptoWatch Status Update", tag=None, plain_body=("STATUS UPDATE: " + status), html_body=None, attachments=None)
+    except postalsend.errors.PostalError as e:
+        logger.error(f"Error sending status update to Postal API: {e}")
